8d0b435e134ae8c450e83c4344ae62cec45d3a05
Eric Yang <ericyang125@gmail.com>
2021-03-09 16:20:28 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import sys
+import os
+
+# append openpiv to pythonpath
+sys.path.append('/home/eric/Repos/openpiv-python')
+
+from openpiv import windef
+from openpiv import tools, scaling, validation, filters, preprocess
+import openpiv.pyprocess as process
+from openpiv import pyprocess
+# from openpiv import widim
+import numpy as np
+import os
+from time import time
+import warnings
+import matplotlib.pyplot as plt
+
+# gets the example directory
+example_dir = os.path.dirname(os.path.abspath(__file__)) + '/example/'
+
+# gets the output directory
+outputs_dir = os.path.dirname(os.path.abspath(__file__)) + '/outputs/'
+
+settings = windef.Settings()
+
+'Data related settings'
+# Folder with the images to process
+# settings.filepath_images = '/home/mira/openpiv-python/openpiv/examples/test8/'
+settings.filepath_images = example_dir
+# Folder for the outputs
+# settings.save_path = '/home/mira/openpiv-python/openpiv/examples/test8/'
+settings.save_path = outputs_dir
+# Root name of the output Folder for Result Files
+settings.save_folder_suffix = 'Test_3'
+
+#  counter = i
+# 'Region of interest'
+# (50,300,50,300) #Region of interest: (xmin,xmax,ymin,ymax) or 'full' for full image
+settings.ROI = 'full'
+
+'Image preprocessing'
+# 'None' for no masking, 'edges' for edges masking, 'intensity' for intensity masking
+# WARNING: This part is under development so better not to use MASKS
+settings.dynamic_masking_method = 'None'
+settings.dynamic_masking_threshold = 0.005
+settings.dynamic_masking_filter_size = 7
+
+settings.deformation_method = 'symmetric'
+
+'Processing Parameters'
+settings.correlation_method = 'circular'  # 'circular' or 'linear'
+settings.normalized_correlation = False
+
+settings.iterations = 2  # select the number of PIV passes
+# add the interroagtion window size for each pass.
+# For the moment, it should be a power of 2
+settings.windowsizes = (64, 32, 16)  # if longer than n iteration the rest is ignored
+# The overlap of the interroagtion window for each pass.
+settings.overlap = (32, 16, 8)  # This is 50% overlap
+# Has to be a value with base two. In general window size/2 is a good choice.
+# methode used for subpixel interpolation: 'gaussian','centroid','parabolic'
+settings.subpixel_method = 'gaussian'
+# order of the image interpolation for the window deformation
+settings.interpolation_order = 3
+settings.scaling_factor = 1  # scaling factor pixel/meter
+settings.dt = 1  # time between to frames (in seconds)
+'Signal to noise ratio options (only for the last pass)'
+# It is possible to decide if the S/N should be computed (for the last pass) or not
+settings.extract_sig2noise = True  # 'True' or 'False' (only for the last pass)
+# method used to calculate the signal to noise ratio 'peak2peak' or 'peak2mean'
+settings.sig2noise_method = 'peak2peak'
+# select the width of the masked to masked out pixels next to the main peak
+settings.sig2noise_mask = 2
+# If extract_sig2noise==False the values in the signal to noise ratio
+# output column are set to NaN
+'vector validation options'
+# choose if you want to do validation of the first pass: True or False
+settings.validation_first_pass = True
+# only effecting the first pass of the interrogation the following passes
+# in the multipass will be validated
+'Validation Parameters'
+# he validation is done at each iteration based on three filters.
+# The first filter is based on the min/max ranges. Observe that these values are defined in
+# terms of minimum and maximum displacement in pixel/frames.
+settings.MinMax_U_disp = (-30, 30)
+settings.MinMax_V_disp = (-30, 30)
+# The second filter is based on the global STD threshold
+settings.std_threshold = 7  # threshold of the std validation
+# The third filter is the median test (not normalized at the moment)
+settings.median_threshold = 3  # threshold of the median validation
+# On the last iteration, an additional validation can be done based on the S/N.
+settings.median_size = 1  # defines the size of the local median
+'Validation based on the signal to noise ratio'
+# Note: only available when extract_sig2noise==True and only for the last
+# pass of the interrogation
+# Enable the signal to noise ratio validation. Options: True or False
+settings.do_sig2noise_validation = False # This is time consuming
+# minmum signal to noise ratio that is need for a valid vector
+settings.sig2noise_threshold = 1.2
+'Outlier replacement or Smoothing options'
+# Replacment options for vectors which are masked as invalid by the validation
+settings.replace_vectors = True  # Enable the replacment. Chosse: True or False
+settings.smoothn = True  # Enables smoothing of the displacemenet field
+settings.smoothn_p = 0.5  # This is a smoothing parameter
+# select a method to replace the outliers: 'localmean', 'disk', 'distance'
+settings.filter_method = 'localmean'
+# maximum iterations performed to replace the outliers
+settings.max_filter_iteration = 4
+settings.filter_kernel_size = 2  # kernel size for the localmean method
+'Output options'
+# Select if you want to save the plo
+#    settings.save_plot = False
+settings.save_plot = True
+# Choose whether you want to see the vectorfield or not :True or False
+settings.show_plot = True
+settings.scale_plot = 200
+# counter += 1
+# select a value to scale the quiver plot of the vectorfield
+
+if __name__ == '__main__':
+    for i in range(250, 255):
+        # Format and Image Sequence
+        settings.frame_pattern_a = '{:04d}_a.tiff'.format(i)
+        settings.frame_pattern_b = '{:04d}_b.tiff'.format(i)
+
+        # run the script with the given settings
+        windef.piv(settings)
